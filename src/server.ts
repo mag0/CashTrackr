31f225e8b78ca374053403dd7df202129065563b
martin <guerreiromartin@gmail.com>
2025-08-30 17:41:36 -0300

    connecting to database

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import colors from 'colors'
+import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.green.bold('Base de datos conectada'));
+    } catch (error) {
+        console.log(colors.red.bold('Error al conectar a la base de datos'), error);
+    }
+}
+
+connectDB();
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+export default app
